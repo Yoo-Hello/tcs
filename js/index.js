00897a01f28657103d730e07fd06ad0da7799172
qiaofan <fan.qiao@51auto.com>
2018-04-09 16:23:21 +0800

    优化了食物出现在身体的BUG
    但是此BUG任然存在，后续优化进行中

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
 $(function(){
+	var sheshen = [],data = {},foods = [],score=0,anima;
+	$('.start').on('click',function(){
+		sheshen=[{x:0,y:0},{x:0,y:1},{x:0,y:2}];
+		data={"0_0":true,"0_1":true,"0_2":true};
+		func();
+		$(this).css({
+			'display':'none'
+		})
+		$('.ing').css({
+			'display':'block'
+		})
+	})
 	var func = function(){
-		$('.tcsbox div').remove();
+		subject();
+		sanke();
+		food();
+		changeDirection();
+		anima = setInterval(function(){move(39)},200);
+	}
+	var score = 0;
+	// 棋盘
+	var subject = function(){
 		var s='';
 		for(var i=0;i<20;i++){
 			for(var j=0;j<20;j++){
@@ -9,75 +29,92 @@ $(function(){
 			}
 		}
 		$('.tcsbox').html(s);
-		var sheshen=[{x:0,y:0},{x:0,y:1},{x:0,y:2}];
-		var data={"0_0":true,"0_1":true,"0_2":true}
-		var huashe=function(){
-			$.each(sheshen,function(index,value){
+	}
+	// 身体
+	var sanke = function(){
+		$.each(sheshen,function(index,value){
 				$('#'+value.x+'_'+value.y).css({"background":"red"})
-			})
-		}
-		huashe();
-
-		//食物
-		var shiwux;
-		var shiwuy;
-		var shiwu = function(){
-			shiwux=Math.floor(Math.random()*19);
-			shiwuy=Math.floor(Math.random()*19);
+			});
+	}
+	//食物
+	var food = function(){
+		foods.shift();
+		var shiwux=Math.floor(Math.random()*19);
+		var shiwuy=Math.floor(Math.random()*19);
+		var judge = function(){
+			console.log('开始判断')
 			for(var i=0;i<sheshen.length;i++){
-				if(shiwux==sheshen[i].x&&shiwuy==sheshen[i].y){
+				console.log(i)
+				if(shiwux!=sheshen[i].x&&shiwuy!=sheshen[i].y){
+					return true;
+				}else{
 					shiwux=Math.floor(Math.random()*19);
 					shiwuy=Math.floor(Math.random()*19);
+					console.log(sheshen[i].x,sheshen[i].y);
+					// i==0;
 				}
 			}
+		}
+		judge();
+		if(judge){
+			
 			$('#'+shiwux+'_'+shiwuy).css({"background":"yellowgreen"});
+			foods.push({x:shiwux,y:shiwuy});
 		}
-		shiwu();
-		//蛇
-		var fangxiang=39;
-		function move(){
-			var oldtou=sheshen[sheshen.length-1];
-			if(fangxiang==39){
-				var newtou={x:oldtou.x,y:oldtou.y+1}
-			}
-			if(fangxiang==37){
-				var newtou={x:oldtou.x,y:oldtou.y-1}
-			}
-			if(fangxiang==38){
-				var newtou={x:oldtou.x-1,y:oldtou.y}
-			}
-			if(fangxiang==40){
-				var newtou={x:oldtou.x+1,y:oldtou.y}
-			}
-
-			if(newtou.x<0||newtou.x>19||newtou.y<0||newtou.y>19||data[newtou.x+'_'+newtou.y]){
-				alert('你撞死了！');
-				clearInterval(zidong);
-				return;
-			}
-			if(newtou.x==shiwux&&newtou.y==shiwuy){
-				shiwu();
-			}else{
-				var weiba=sheshen.shift();
-				delete data[weiba.x+'_'+weiba.y];
-				$('#'+weiba.x+'_'+weiba.y).css({"background":"#ccc"})
-			}
-			sheshen.push(newtou);
-			$('#'+newtou.x+'_'+newtou.y).css({"background":"red"});
-			data[newtou.x+'_'+newtou.y]=true;
-			console.log(data);
-		}
-		var zidong = setInterval(move,200);
-
-		$(window).keydown(function(e){
-			if(Math.abs(e.keyCode-fangxiang) == 2){
-				return;
-			}
-			fangxiang=e.keyCode;
-		})
+		
 	}
-	func();
-	$('.btn').on('click',function(){
-		func();
-	})
+	// 按键改变方向
+	var changeDirection = function(animation){
+		var initialKey = 39;
+		$(window).keydown(function(e){
+			if(e.keyCode !== 37 && e.keyCode !== 38 && e.keyCode !== 39 && e.keyCode !== 40){
+				return;
+			}else if(Math.abs(e.keyCode-initialKey) !== 2){
+ 				clearInterval(anima);
+ 				anima = setInterval(function(){move(e.keyCode)},200);
+ 				initialKey = e.keyCode
+ 			}
+ 		});
+	}
+ 	//蛇移动
+ 	var move = function( k ){
+ 		var initialKey = k;
+ 		var oldHead=sheshen[sheshen.length-1];
+ 		if(initialKey == 39){
+ 			var newHead={x:oldHead.x,y:oldHead.y+1}
+ 		}
+ 		if(initialKey == 37){
+ 			var newHead={x:oldHead.x,y:oldHead.y-1}
+ 		}
+ 		if(initialKey == 38){
+ 			var newHead={x:oldHead.x-1,y:oldHead.y}
+ 		}
+ 		if(initialKey == 40){
+ 			var newHead={x:oldHead.x+1,y:oldHead.y}
+ 		}
+ 		if(newHead.x<0||newHead.x>19||newHead.y<0||newHead.y>19||data[newHead.x+'_'+newHead.y]){
+ 			if(confirm('你撞死了！')){
+ 				$('.start').css({'display':'block'});
+ 				$('.ing').css({'display':'none'});
+ 			};
+ 			clearInterval(anima);
+ 			return false;
+ 		}
+ 		if(newHead.x==foods[0].x&&newHead.y==foods[0].y){
+ 			food();
+ 			score++;
+ 			$('.score p span').html(score);
+ 		}else{
+ 			var weiba=sheshen.shift();
+ 			delete data[weiba.x+'_'+weiba.y];
+ 			$('#'+weiba.x+'_'+weiba.y).css({"background":"#ccc"})
+ 		}
+ 		$('#'+newHead.x+'_'+newHead.y).css({"background":"red"});
+ 		data[newHead.x+'_'+newHead.y]=true;
+ 		sheshen.push(newHead);
+ 	}
+// 	var DisplayScore = function(){
+// 		$('.score p span').html(score);
+// 	}
+// 	DisplayScore();
 })
